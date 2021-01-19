02389ec9557284d72f4be1d8f363f9a7ec0ceb60
Jose Lopez <joselopez11394@gmail.com>
2021-01-19 17:35:17 -0500

    fixes and add more test

--- spec/service/geo_map_api_spec.rb
+++ spec/service/geo_map_api_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe GeoMapApi do
     it "geo_location to grab lat and lng" do
       VCR.use_cassette("geo_location") do
         location = GeoMapApi.geo_location("denver,co")
+
         expect(location).to be_a(Hash)
+        expect(location).to include(:lat)
+        expect(location).to include(:lng)
         expect(location[:lat]).to be_a(Float)
         expect(location[:lng]).to be_a(Float)
       end
@@ -14,6 +17,11 @@ RSpec.describe GeoMapApi do
     it "gets the real time to travel" do
       VCR.use_cassette("road_trip") do
         location = GeoMapApi.travel("denver,co", "Estes Park, CO")
+
+        expect(location).to be_a(Hash)
+        expect(location).to include(:route)
+        expect(location[:route]).to be_a(Hash)
+        expect(location[:route]).to include(:realTime)
         expect(location[:route][:realTime]).to be_a_kind_of(Numeric)
       end
     end
@@ -22,6 +30,11 @@ RSpec.describe GeoMapApi do
       VCR.use_cassette("impossible_road_trip") do
         location = GeoMapApi.travel("denver,co", "japan")
 
+        expect(location).to be_a(Hash)
+        expect(location).to include(:info)
+        expect(location[:info]).to be_a(Hash)
+        expect(location[:info]).to include(:messages)
+        expect(location[:info][:messages]).to be_a(Array)
         expect(location[:info][:messages][0]).to eq("We are unable to route with the given locations.")
       end
     end
