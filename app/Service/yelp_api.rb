74780660c748204707b8dae4f2511ea7a9d0cd19
Jose Lopez <joselopez11394@gmail.com>
2021-01-19 12:56:29 -0500

    adds unix to yelp api call and changes facade

--- app/Service/yelp_api.rb
+++ app/Service/yelp_api.rb
@@ -1,9 +1,9 @@
 class YelpApi
-  def self.resturants(location, food)
+  def self.resturants(location, food, unix)
     response = conn.get("/v3/businesses/search?") do |req|
       req.params[:location] = location
       req.params[:term] = food
-
+      req.params[:open_at] = unix
     end
     JSON.parse(response.body, symbolize_names: true)    
   end
--- app/facade/munchies_search_facade.rb
+++ app/facade/munchies_search_facade.rb
@@ -3,7 +3,6 @@ class MunchiesSearchFacade
     travel_time = GeoMapApi.travel(start_city, end_city)
     location = GeoMapApi.geo_location(end_city)
     weather = WeatherApi.weather_forecast(location[:lat], location[:lng])
-    resturants = YelpApi.resturants(end_city, food)
-    Munchie.new(travel_time, weather, end_city, resturants)
+    Munchie.new(travel_time, weather, end_city, food)
   end
 end
